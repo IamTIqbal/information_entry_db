1641b63f385df50ea4f49035b3ff74e7f7ff9ef8
Tamim Iqbal <108043975+IamTIqbal@users.noreply.github.com>
2022-06-30 19:55:14 +0600

    main

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-
+<?php
+include 'config/database.php';
+mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Information Entry</title>
+    <link href="css/main.css" />
+</head>
+<body>
+    
+</body>
+</html>
